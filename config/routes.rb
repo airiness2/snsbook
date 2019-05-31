d807056ed05232d29b00ace0e9c3e3c8a440359e
reiko watanabe <blackgothy@gmail.com>
2019-05-31 07:42:13 +0000

    投稿画面基本を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy]
     resources :users
+    
+    resources :posts do
+        collection do
+            post :confirm
+        end
+    end
 end
