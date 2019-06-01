0ea107906693dbb06178ace7a9fad3645ff9838b
reiko watanabe <blackgothy@gmail.com>
2019-06-01 06:46:58 +0000

    ログインしていなかった場合にログインページに誘導

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
     
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   
+  before_action :sign_in_user
+  
   def index
     @posts = Post.all
   end
@@ -60,5 +62,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   
+  def sign_in_user
+    redirect_to new_session_path unless logged_in?
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
-config.log_level = :info
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +17,10 @@ module Snsbook
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
+  config.log_level = :info
+
   end
 end
