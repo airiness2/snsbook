8a477effc306eea59ff20298088d672d96dbdf09
reiko watanabe <blackgothy@gmail.com>
2019-06-01 01:14:21 +0000

    画像アップロード機能搭載

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, 
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
-    before_validation { email.downcase! }
+#    before_validation { email.downcase! }
     
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
-
+    
+    has_many :posts
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190531073239) do
+ActiveRecord::Schema.define(version: 20190601000120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20190531073239) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.text "image"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,6 +30,8 @@ ActiveRecord::Schema.define(version: 20190531073239) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
