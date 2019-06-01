01ffaf4c1d86ce8dd8b063c8df2288b3720d2a92
reiko watanabe <blackgothy@gmail.com>
2019-06-01 02:30:56 +0000

    日本語化

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.time_zone = 'Tokyo'
+config.active_record.default_timezone = :local
+config.log_level = :info
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
